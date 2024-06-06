e59d2eadc75976656a35a594abba67c5265551f5
Diniru Kahaduwa Arachchi <diniruchamodya@gmail.com>
2024-06-07 00:00:53 +0530

    Your commit message

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,6 @@
 
 public class Helloworld { // reserved keyword
-    public static void main(String[] args) { // java-compiler-bitecode-interpreter
+    public static void main(String[] args) { // java-compiler-bytecode-interpreter
         System.out.println("Hello World");
     }
 }
-
