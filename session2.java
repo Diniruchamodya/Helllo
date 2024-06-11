9b6586c95627e6ef72965d36c3530af08be221b8
Diniru Kahaduwa Arachchi <diniruchamodya@gmail.com>
2024-06-11 09:24:46 +0530

    Add files via upload

---
+++ session2.java
@@ -0,0 +1,19 @@
+public class session2 {
+    public static void main(String[] args) {
+
+        int Intvalue = 9; // <-statement
+        // -- Primitive data types--
+        // byte bytevalue = 123;
+        // short shortvalue = 324;
+        // boolean booleanvalue = true;
+        // char charvalue = 'A';
+        // int intvalue = 9;
+
+        // --Non primitive data types--
+        // String stringvalue = "Hello";
+        // integer
+        // arrays
+
+    }
+
+}
